dd71b6c57f8a59220cf285cf7e0978c3cbbe3a90
Franck Ernewein <franck.ernewein@swile.co>
2023-03-13 11:43:19 +0100

    add script to init db

--- src/functions/createCollection/handler.ts
+++ src/functions/createCollection/handler.ts
@@ -1,8 +1,33 @@
-import { formatJSONResponse } from '@libs/api-gateway';
-import { middyfy } from '@libs/lambda';
+import { formatJSONResponse } from "@libs/api-gateway";
+import { middyfy } from "@libs/lambda";
+import {
+  DynamoDBClient,
+  CreateTableCommand,
+  BillingMode,
+  KeyType,
+  ScalarAttributeType,
+  ListTablesCommand,
+} from "@aws-sdk/client-dynamodb";
+
+let ddb: any;
+function getDdbClient() {
+  if (!ddb) {
+    ddb = new DynamoDBClient({
+      credentials: {
+        accessKeyId: "fake-key",
+        secretAccessKey: "fake-secret",
+      },
+      endpoint: "http://localhost:8001",
+      region: "local",
+    });
+  }
+  return ddb;
+}
 
 const createCollection = async (event) => {
-
+  await getDdbClient().putItem({
+    Item: {},
+  });
   return formatJSONResponse({});
 };
 
